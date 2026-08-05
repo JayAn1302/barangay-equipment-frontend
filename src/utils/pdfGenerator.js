9d58f7c7e46c2780468fee922185b7b649f65074
jay-an <jay-an_calago@dmc.edu.ph>
2026-08-05 22:46:15 +0800

    www

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,11 +1,9 @@
-
-import autoTable from "jspdf-autotable";
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import seal from "../assets/seal.png";
 
 export function generatePdf(title, columns, rows, print = false) {
     const doc = new jsPDF();
-
-    const { jsPDF } = window;
 
     const fullName =
         localStorage.getItem("fullName") || "Administrator";
