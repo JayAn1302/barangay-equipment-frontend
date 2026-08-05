5da7bd602f8b40b1bd80a18d3bd76116ad0d77ba
jay-an <jay-an_calago@dmc.edu.ph>
2026-08-05 22:52:17 +0800

    pp

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['jspdf', 'jspdf-autotable']
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
